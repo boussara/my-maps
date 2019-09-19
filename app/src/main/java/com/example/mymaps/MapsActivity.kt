e38366b00c3eb81557d7f1cec518fe01b8101a95
Mona Lisa <Mona@gmail.com>
2019-09-19 13:58:16 +0100

    with icon

--- app/src/main/java/com/example/mymaps/MapsActivity.kt
+++ app/src/main/java/com/example/mymaps/MapsActivity.kt
@@ -1,5 +1,7 @@
 package com.example.mymaps
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,6 +9,8 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
@@ -37,20 +41,39 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val extraMarkers: ArrayList<LatLng> = ArrayList()
 
 
-        extraMarkers.add(LatLng(33.9976735,-6.8486683))
-        extraMarkers.add(LatLng(33.9984874,-6.8490457))
+        extraMarkers.add(LatLng(34.04595025000031,-6.812889330000002))
+        extraMarkers.add(LatLng(34.0459318900003,-6.812878999999999))
+        extraMarkers.add(LatLng(33.9977420300003,-6.849294970000001))
+        extraMarkers.add(LatLng(33.4288065800004,-6.00299268))
+        extraMarkers.add(LatLng(33.4286172500004,-6.00307351))
+        extraMarkers.add(LatLng(33.428354060000395,-6.0030923099999995))
+        extraMarkers.add(LatLng(33.428173510000384,-6.00336325))
+        extraMarkers.add(LatLng(33.4280136900004,-6.003446079999999))
+        extraMarkers.add(LatLng(33.42785644000038,-6.0035418300000005))
+        extraMarkers.add(LatLng(33.4279184800004,-6.003374609999998))
+        extraMarkers.add(LatLng(33.427919350000394,-6.0031679))
+        extraMarkers.add(LatLng(33.42785223000039,-6.002971860000001))
+        extraMarkers.add(LatLng(33.396240530000384,-6.08468802))
+        extraMarkers.add(LatLng(33.396111570000386,-6.08486365))
+        extraMarkers.add(LatLng(33.39341960000038,-6.09080054))
+        extraMarkers.add(LatLng(33.39357703000039,-6.0909405))
+        extraMarkers.add(LatLng(33.425138810000405,-6.005682909999998))
+        extraMarkers.add(LatLng(33.43108673000039,-6.00257426))
         // Add a marker in Sydney and move the camera
        // val sydney = LatLng(33.9976735,-6.8486683)
 
         for (latLng in extraMarkers)
         {
+
+
             mMap.addMarker(MarkerOptions()
                 .position(latLng)
-                .title("Mrhba bik")
-                .snippet("Mrhba bik")
+                .title("Groupe Derichebourg")
+                .snippet("Commune D'Oulmes")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.z))
 
             )
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,14f))
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,16f))
         }
 
 
